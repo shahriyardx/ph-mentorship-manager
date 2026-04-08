82c97c9b7a51e8305aadb41596293619d5a58271
Shahriyar <mdshahriyaralam552@gmail.com>
2026-04-08 13:15:27 +0600

    added user search feature

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -10,8 +10,11 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useMemo, useState } from "react"
 
 const page = () => {
+  const [search, setSearch] = useState("")
   const { data: users, isPending, refetch } = trpc.admin.users.useQuery()
   const { mutate: makeAdmin } = trpc.admin.makeAdmin.useMutation({
     onSuccess: () => {
@@ -24,12 +27,31 @@ const page = () => {
     },
   })
 
+  const filteredUsers = useMemo(() => {
+    if (!users) return []
+    if (!search) return users
+    return users.filter(
+      (user) =>
+        user.name.toLowerCase().includes(search.toLowerCase()) ||
+        user.email.toLowerCase().includes(search.toLowerCase()),
+    )
+  }, [search, users])
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-3">Users</h1>
       {isPending && <p className="mb-3">Loading...</p>}
-      {users && <p>Total: {users.length}</p>}
-      <Table className="mt-5 border-2">
+
+      <div className="mt-5 flex items-center  justify-between">
+        {users && <p>Total: {users.length}</p>}
+        <Input
+          placeholder="Name or Email"
+          className="w-full max-w-80"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <Table className="mt-2 border-2">
         <TableHeader>
           <TableRow>
             <TableHead>Name</TableHead>
@@ -39,7 +61,7 @@ const page = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {users?.map((user) => (
+          {filteredUsers.map((user) => (
             <TableRow key={user.id}>
               <TableCell>{user.name}</TableCell>
               <TableCell>{user.email}</TableCell>
