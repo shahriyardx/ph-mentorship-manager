dc8f4bf06044ee075488c4220e3d60c0fce4deb6
Shahriyar <mdshahriyaralam552@gmail.com>
2026-04-23 11:39:16 +0600

    updated settings ui

--- src/app/(dashboard)/admin/settings/page.tsx
+++ src/app/(dashboard)/admin/settings/page.tsx
@@ -2,7 +2,11 @@
 
 import { DashboardPageWrapper } from "@/components/dashboard-page-wrapper"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
 import { trpc } from "@/trpc/client"
+import { toast } from "sonner"
 
 const page = () => {
   const { data: settings, refetch } = trpc.admin.settings.useQuery()
@@ -11,15 +15,27 @@ const page = () => {
       onSuccess: () => {
         refetch()
       },
+      onError: (error) => {
+        toast.error(error.message)
+      },
     })
   return (
     <DashboardPageWrapper pageTitle="Settings">
       <div>
-        <Button onClick={() => toggleMaintenanceMode()}>
-          {settings?.maintenanceMode
-            ? "Disable Maintenance Mode"
-            : "Enable Maintenance Mode"}
-        </Button>
+        <Card className="max-w-md">
+          <CardHeader>
+            <CardTitle>Settings</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex justify-between">
+              <Label>Maintenance Mode</Label>
+              <Switch
+                checked={settings?.maintenanceMode}
+                onCheckedChange={() => toggleMaintenanceMode()}
+              />
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </DashboardPageWrapper>
   )
